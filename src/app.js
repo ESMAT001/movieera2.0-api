7e583cb969dcb5bfa94dcaaffcd264516b853595
ESMAT001 <esmat10.niazi@gmail.com>
2022-06-25 00:46:34 +0430

    new files added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import createError from 'http-errors';
 import express from 'express';
 import path from 'path';
-import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 
 import indexRouter from '#routes/index';
@@ -14,7 +13,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 
 app.use('/', indexRouter);
